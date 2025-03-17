90b4965d57dfb4ed5ca34335b20682c566c660f8
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-17 14:47:02 -0400

    Delete src directory

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "gui.hpp"
-#include <gtkmm/application.h>
-
-int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "org.ps1.emulator");
-    GUI gui;
-    return app->run(gui);
-}
