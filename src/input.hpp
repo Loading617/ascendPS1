8198cd10ff3c16cb804f4a17917250d010e9eaac
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-25 18:36:10 -0400

    Update input.hpp

--- src/input.hpp
+++ src/input.hpp
@@ -1,50 +1,39 @@
 #ifndef INPUT_HPP
 #define INPUT_HPP
 
-#include <cstdint>
-#include <unordered_map>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_haptic.h>
+#include <map>
 #include <string>
-#include <vector>
 
 class Input {
 public:
     Input();
     ~Input();
 
-    void update();
+    void processEvent(const SDL_Event& event);
     uint16_t getButtonState() const;
-    void setButtonState(uint16_t state);
-
-    void setVibration(float strength, uint32_t duration);
-    void configureKey(SDL_Keycode key, uint16_t psButton);
-    void saveKeyMappings(const std::string& filename);
-    void loadKeyMappings(const std::string& filename);
-    void loadProfile(const std::string& profileName);
-    void saveProfile(const std::string& profileName);
-    std::vector<std::string> getAvailableProfiles();
+    int8_t getAnalogLX() const;
+    int8_t getAnalogLY() const;
+    int8_t getAnalogRX() const;
+    int8_t getAnalogRY() const;
 
     void openRemapUI();
-
-    int16_t getAnalogLX() const;
-    int16_t getAnalogLY() const;
-    int16_t getAnalogRX() const;
-    int16_t getAnalogRY() const;
-    void setAnalogSensitivity(float sensitivity);
+    void saveKeyMappings(const std::string& filename);
+    void loadKeyMappings(const std::string& filename);
 
 private:
-    uint16_t buttonState;
-    int16_t analogLX, analogLY, analogRX, analogRY;
-    float analogSensitivity;
+    void initKeyMap();
 
-    std::unordered_map<SDL_Keycode, uint16_t> keyMap;
+    uint16_t buttonState;
+    int8_t analogLX, analogLY, analogRX, analogRY;
+    float analogSensitivity;
     SDL_GameController* controller;
     SDL_Haptic* haptic;
-
-    void initKeyMap();
-    void handleKeyboard();
-    void handleGamepad();
+    
+    std::map<SDL_Keycode, uint16_t> keyMap;
+    
+    void saveProfile(const std::string& name);
+    void loadProfile(const std::string& name);
 };
 
 #endif
