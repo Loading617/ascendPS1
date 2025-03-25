33bfc87e86bc0434f5497020a7a3efb369a45097
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-25 17:28:14 -0400

    Update input.hpp

--- src/input.hpp
+++ src/input.hpp
@@ -5,6 +5,8 @@
 #include <unordered_map>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_haptic.h>
+#include <string>
+#include <vector>
 
 class Input {
 public:
@@ -17,15 +19,24 @@ public:
 
     void setVibration(float strength, uint32_t duration);
     void configureKey(SDL_Keycode key, uint16_t psButton);
+    void saveKeyMappings(const std::string& filename);
+    void loadKeyMappings(const std::string& filename);
+    void loadProfile(const std::string& profileName);
+    void saveProfile(const std::string& profileName);
+    std::vector<std::string> getAvailableProfiles();
+
+    void openRemapUI();
 
     int16_t getAnalogLX() const;
     int16_t getAnalogLY() const;
     int16_t getAnalogRX() const;
     int16_t getAnalogRY() const;
+    void setAnalogSensitivity(float sensitivity);
 
 private:
     uint16_t buttonState;
     int16_t analogLX, analogLY, analogRX, analogRY;
+    float analogSensitivity;
 
     std::unordered_map<SDL_Keycode, uint16_t> keyMap;
     SDL_GameController* controller;
