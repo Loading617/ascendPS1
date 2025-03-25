48f7e652accdb753f5b32fed150765bb8e9ce3c7
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-25 15:29:22 -0400

    Update input.hpp

--- src/input.hpp
+++ src/input.hpp
@@ -2,16 +2,27 @@
 #define INPUT_HPP
 
 #include <cstdint>
+#include <unordered_map>
+#include <SDL2/SDL.h>
 
 class Input {
 public:
     Input();
+    ~Input();
+
     void update();
     uint16_t getButtonState() const;
     void setButtonState(uint16_t state);
 
 private:
     uint16_t buttonState;
+
+    std::unordered_map<SDL_Keycode, uint16_t> keyMap;
+    SDL_GameController* controller;
+
+    void initKeyMap();
+    void handleKeyboard();
+    void handleGamepad();
 };
 
 #endif
