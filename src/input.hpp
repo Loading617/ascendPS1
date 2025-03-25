aab0368052cf3bd55c488d6b0b27552805ccf583
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-25 16:38:49 -0400

    Update input.hpp

--- src/input.hpp
+++ src/input.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <unordered_map>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_haptic.h>
 
 class Input {
 public:
@@ -14,11 +15,21 @@ public:
     uint16_t getButtonState() const;
     void setButtonState(uint16_t state);
 
+    void setVibration(float strength, uint32_t duration);
+    void configureKey(SDL_Keycode key, uint16_t psButton);
+
+    int16_t getAnalogLX() const;
+    int16_t getAnalogLY() const;
+    int16_t getAnalogRX() const;
+    int16_t getAnalogRY() const;
+
 private:
     uint16_t buttonState;
+    int16_t analogLX, analogLY, analogRX, analogRY;
 
     std::unordered_map<SDL_Keycode, uint16_t> keyMap;
     SDL_GameController* controller;
+    SDL_Haptic* haptic;
 
     void initKeyMap();
     void handleKeyboard();
