0caeb7626f47e70972deb8148c1e9bf68332c3a9
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-31 22:54:13 -0400

    Delete src directory

--- src/utils.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <cstdint>
-#include <fstream>
-
-std::vector<uint8_t> load_file(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary);
-    return std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
-}
--- src/utils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
-#include <vector>
-#include <string>
-
-std::vector<uint8_t> load_file(const std::string& filename);
-
-#endif
