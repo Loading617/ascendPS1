b5b35a3c73335734970f8103840c09966ffbea9b
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-05-26 16:07:39 -0400

    Delete bios directory

--- bios/bios.cpp
+++
@@ -1,7 +0,0 @@
-#include "bios.hpp"
-#include <fstream>
-
-std::vector<uint8_t> BIOS::load(const std::string& path) {
-    std::ifstream file(path, std::ios::binary);
-    return std::vector<uint8_t>(std::istreambuf_iterator<char>(file), {});
-}
--- bios/bios.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-class BIOS {
-public:
-    static std::vector<uint8_t> load(const std::string& path);
-};
